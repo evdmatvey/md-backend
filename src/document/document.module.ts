8e5306c36a8750cf89a4091d472698770487fcb3
evdmatvey <evd.matvey@mail.ru>
2025-11-23 15:23:53 +0700

    Initialize document typeorm entity

---
+++ src/document/document.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DocumentController } from './document.controller';
+import { DocumentService } from './document.service';
+import { DocumentEntity } from './entities/document.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([DocumentEntity])],
+  controllers: [DocumentController],
+  providers: [DocumentService],
+})
+export class DocumentModule {}
