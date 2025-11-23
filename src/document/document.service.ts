acd2c12ac8202a7f7cbfa13e6904c5f18d74303f
evdmatvey <evd.matvey@mail.ru>
2025-11-23 16:58:40 +0700

    Add create method to document service

--- src/document/document.service.ts
+++ src/document/document.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { nanoid } from 'nanoid';
+import { QueryFailedError, Repository } from 'typeorm';
+import { CreateDocumentDto } from './dto/create-document.dto';
+import { DocumentEntity } from './entities/document.entity';
 
 @Injectable()
-export class DocumentService {}
+export class DocumentService {
+  private static readonly SLUG_LENGTH = 8;
+  private static readonly NON_UNIQUE_ERROR_CODE = '23505';
+
+  public constructor(
+    @InjectRepository(DocumentEntity)
+    private readonly _documentRepository: Repository<DocumentEntity>,
+  ) {}
+
+  public async create(dto: CreateDocumentDto): Promise<DocumentEntity> {
+    while (true) {
+      const slug = nanoid(DocumentService.SLUG_LENGTH);
+
+      const document = this._documentRepository.create({
+        ...dto,
+        slug,
+      });
+
+      try {
+        return this._documentRepository.save(document);
+      } catch (error) {
+        if (this._isNonUniqueSlugGenerated(error)) continue;
+        throw error;
+      }
+    }
+  }
+
+  private _isNonUniqueSlugGenerated(error: any) {
+    return (
+      error instanceof QueryFailedError &&
+      (error as QueryFailedError & { code: string }).code ===
+        DocumentService.NON_UNIQUE_ERROR_CODE
+    );
+  }
+}
