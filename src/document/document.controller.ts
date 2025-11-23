f5ca5835e4845b479f4478834ef801b05bfac5ca
evdmatvey <evd.matvey@mail.ru>
2025-11-23 17:04:47 +0700

    Add create endpoint to document controller

--- src/document/document.controller.ts
+++ src/document/document.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { DocumentService } from './document.service';
+import { CreateDocumentDto } from './dto/create-document.dto';
 
 @Controller('documents')
 export class DocumentController {
-  constructor(private readonly _documentService: DocumentService) {}
+  public constructor(private readonly _documentService: DocumentService) {}
+
+  @Post()
+  @HttpCode(201)
+  @UsePipes(new ValidationPipe())
+  public async create(@Body() dto: CreateDocumentDto) {
+    return this._documentService.create(dto);
+  }
 }
