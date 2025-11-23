0d9aeb2fcc3b9e31e9200bbc13a852c31dd04a72
evdmatvey <evd.matvey@mail.ru>
2025-11-23 17:15:37 +0700

    Add swagger documentation for create document endpoint

--- src/document/document.controller.ts
+++ src/document/document.controller.ts
@@ -6,9 +6,20 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { DocumentService } from './document.service';
 import { CreateDocumentDto } from './dto/create-document.dto';
+import {
+  DocumentBadRequestResponse,
+  DocumentOkResponse,
+} from './swagger/response.types';
 
+@ApiTags('Документы')
 @Controller('documents')
 export class DocumentController {
   public constructor(private readonly _documentService: DocumentService) {}
@@ -16,6 +27,16 @@ export class DocumentController {
   @Post()
   @HttpCode(201)
   @UsePipes(new ValidationPipe())
+  @ApiOperation({ summary: 'Создание короткой ссылки на markdown' })
+  @ApiOkResponse({
+    type: DocumentOkResponse,
+    description: 'Короткая ссылка на переданный markdown успешно создана',
+  })
+  @ApiBadRequestResponse({
+    type: DocumentBadRequestResponse,
+    description:
+      'Данные для создания короткой ссылки на markdown были переданы неверно',
+  })
   public async create(@Body() dto: CreateDocumentDto) {
     return this._documentService.create(dto);
   }
--- src/document/dto/create-document.dto.ts
+++ src/document/dto/create-document.dto.ts
@@ -1,11 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class CreateDocumentDto {
+  @ApiProperty({
+    required: true,
+    example: 'README',
+    description: 'Название',
+  })
   @IsString({ message: 'Название должно быть строкой.' })
   @IsNotEmpty({ message: 'Название не должно быть пустым.' })
   @MaxLength(255, { message: 'Название слишком длинное.' })
   title: string;
 
+  @ApiProperty({
+    required: true,
+    example: '## Hello, world!',
+    description: 'Текст в формате markdown',
+  })
   @IsString({ message: 'Markdown должен быть строкой.' })
   @IsNotEmpty({ message: 'Markdown не должен быть пустым.' })
   markdown: string;
