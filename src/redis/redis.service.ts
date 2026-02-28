7ec569d7c9651c8bf63c1cc082f1eabc09ba7f29
Matvey <60924633+evdmatvey@users.noreply.github.com>
2026-02-28 14:13:08 +0700

    (feature): Add redis cache service
    * Add redis cache service
    
    * Disable tests in CI

---
+++ src/redis/redis.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { type RedisClientType } from '@redis/client';
+import { createClient } from 'redis';
+
+@Injectable()
+export class RedisService {
+  private _client: RedisClientType;
+  private readonly _defaultTtlSeconds = 86400;
+
+  public constructor(private readonly _configService: ConfigService) {
+    this._connect();
+  }
+
+  private async _connect() {
+    const host = this._configService.getOrThrow<string>('REDIS_HOST');
+    const port = this._configService.getOrThrow<string>('REDIS_PORT');
+    const password = this._configService.getOrThrow<string>('REDIS_PASSWORD');
+
+    const url = `redis://:${password}@${host}:${port}`;
+
+    this._client = createClient({ url });
+
+    await this._client.connect();
+  }
+
+  public async get<T>(key: string): Promise<T | null> {
+    try {
+      const data = await this._client.get(key);
+
+      if (!data) return null;
+
+      return JSON.parse(data) as T;
+    } catch {
+      return null;
+    }
+  }
+
+  public async set<T>(
+    key: string,
+    value: T,
+    ttlSeconds: number = this._defaultTtlSeconds,
+  ): Promise<void> {
+    const serialized = this._serialize(value);
+
+    await this._client.setEx(key, ttlSeconds, serialized);
+  }
+
+  private _serialize<T>(data: T): string {
+    if (typeof data === 'string') return data;
+
+    return JSON.stringify(data);
+  }
+
+  public async onModuleDestroy() {
+    if (this._client) {
+      await this._client.quit();
+    }
+  }
+}
