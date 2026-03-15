c4b2e6967ede0da6503d7d5dfed6b0ad32e3ac71
evdmatvey <evd.matvey@mail.ru>
2026-03-15 23:34:26 +0700

    Export new entities from entities public api

--- src/domains/entities/index.ts
+++ src/domains/entities/index.ts
@@ -1,3 +1,6 @@
 export { Document } from './document.entity';
 export { ModerationAction } from './moderation-action.entity';
 export { ModerationActionHistory } from './moderation-action-history.entity';
+export { User } from './user.entity';
+export { RoleAssignmentHistory } from './role-assignment-history.entity';
+export { RoleAssignmentMetadata } from './role-assignment-metadata.entity';
