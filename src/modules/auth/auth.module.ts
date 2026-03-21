423227c967ab48d2878c7513c03de64981c847ef
evdmatvey <evd.matvey@mail.ru>
2026-03-21 14:08:28 +0700

    Add session entity

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
+import { SessionEntity } from './entities/session.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([SessionEntity])],
   controllers: [AuthController],
   providers: [],
 })
